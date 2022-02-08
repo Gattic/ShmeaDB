5e8738aa6e6faad2a586cbd537d37956af8f0e4a
Lerring <Lee.Herring97@gmail.com>
2022-02-08 00:13:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 {
   cmake ../ 
 } || {
-  echo "ERROR!"
+  echo "OUTPUT!"
   echo "$(cat /ShmeaDB/build/CMakeFiles/CMakeOutput.log)"
+  echo "ERROR!"
   echo "$(cat /ShmeaDB/build/CMakeFiles/CMakeError.log)"
   exit 1
 }
