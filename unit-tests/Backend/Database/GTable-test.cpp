e80c2e5e2c419ffae92518b498d7e54506a4fe4f
Glen Rosen <njmedic2669@gmail.com>
2024-08-20 09:53:49 -0400

    Added basic save/load unit test

--- unit-tests/Backend/Database/GTable-test.cpp
+++ unit-tests/Backend/Database/GTable-test.cpp
@@ -9,6 +9,7 @@
 #include "../../unit-test.h"
 #include "../../../Backend/Database/GTable.h"
 #include "../../../Backend/Database/GString.h"
+#include "../../../Backend/Database/GObject.h"
 
 // This File will have the more advanced functionalities of GTable
 // For simpler tests check GList-test.cpp
@@ -18,5 +19,17 @@
 
 void GTableUnitTest()
 {
-	//
+	shmea::GString fname( "AAPLtestTable.csv" );
+	shmea::GString outputFile( "outputTest.csv" );
+	shmea::GTable testTable( fname, ',', shmea::GTable::TYPE_FILE );
+
+	G_assert(__FILE__, __LINE__, "==============GTable::getNumCols() Failed==============",  testTable.numberOfCols() == 6);
+	G_assert(__FILE__, __LINE__, "==============GTable::getNumRows() Failed==============", testTable.numberOfRows() == 500 );
+
+	//testTable.print();
+	testTable.save( outputFile );
+
+	shmea::GTable readTable( outputFile, ',', shmea::GTable::TYPE_FILE );
+	G_assert(__FILE__, __LINE__, "==============GTable::getNumCols() Comparison Failed==============",  readTable.numberOfCols() == testTable.numberOfCols() );
+	G_assert(__FILE__, __LINE__, "==============GTable::getNumRows() Comparison Failed==============", readTable.numberOfRows() == testTable.numberOfRows() );
 }
