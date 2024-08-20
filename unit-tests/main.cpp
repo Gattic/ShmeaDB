b6d91ada81e4355bdb5da3ba5adf120a6d9373c7
Glen Rosen <njmedic2669@gmail.com>
2024-08-20 09:54:26 -0400

    Added GTable unit test

--- unit-tests/main.cpp
+++ unit-tests/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
 	GVectorUnitTest();
 	GPointerUnitTest();
 	GListUnitTest();
-	//GTableUnitTest();
+	GTableUnitTest();
 	//GObjectsUnitTest();
 	CryptUnitTest();
 
