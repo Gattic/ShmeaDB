cc51f97687355124a612f7307e858a1d75fafb61
cabral450 <robert.carneiro352@gmail.com>
2025-02-23 10:37:07 -0500

    WASM compatability

---
+++ wasm.sh
@@ -0,0 +1,32 @@
+mkdir build
+cd build
+emcmake cmake ..
+emmake make -j4
+emmake make install
+
+mkdir GNetObjects
+cd GNetObjects
+emar x ../Backend/Networking/libGNet.a
+cd ..
+echo "Extracted GNet Objects"
+
+mkdir GDBObjects
+cd GDBObjects
+emar x ../Backend/Database/libDB.a
+cd ..
+echo "Extracted GDB Objects"
+
+mkdir GObjects
+cd GObjects
+emar x ../libshmea.a
+cd ..
+echo "Extracted G Objects"
+
+emar rcs libshmea_combined.a GObjects/*.o GDBObjects/*.o GNetObjects/*.o
+echo "Combined G Objects into libshmea_combined.a"
+
+cp libshmea_combined.a $HOME/.local/lib/libshmea.a
+echo "Copied libshmea_combined.a to $HOME/.local/lib/libshmea.a"
+
+rm ~/.local/lib/libshmea.so
+echo "Removed ~/.local/lib/libshmea.so (if it exists, not allowed to exist for emsdk)"
