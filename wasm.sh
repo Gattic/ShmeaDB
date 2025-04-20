3a7fa39edbfb63baeae6a2006a93e3c4c90d70d7
cabral450 <robert.carneiro352@gmail.com>
2025-04-20 12:11:17 -0400

    WASM compile plotter

--- wasm.sh
+++ wasm.sh
@@ -16,13 +16,19 @@ emar x ../Backend/Database/libDB.a
 cd ..
 echo "Extracted GDB Objects"
 
+mkdir GPlotterObjects
+cd GPlotterObjects
+emar x ../Backend/Plotter/libPlotter.a
+cd ..
+echo "Extracted GPlotter Objects"
+
 mkdir GObjects
 cd GObjects
 emar x ../libshmea.a
 cd ..
 echo "Extracted G Objects"
 
-emar rcs libshmea_combined.a GObjects/*.o GDBObjects/*.o GNetObjects/*.o
+emar rcs libshmea_combined.a GObjects/*.o GDBObjects/*.o GPlotterObjects/*.o GNetObjects/*.o
 echo "Combined G Objects into libshmea_combined.a"
 
 cp libshmea_combined.a $HOME/.local/lib/libshmea.a
